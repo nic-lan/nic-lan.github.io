00d8e6af05af1c1cba049bf236fc7450bd414d9a
nic-lan <nicolas.languille@gmail.com>
2017-03-12 14:45:04 +0100

    Expect linked in link to be on the page

--- test/features/show_homepage_test.exs
+++ test/features/show_homepage_test.exs
@@ -2,6 +2,8 @@ defmodule ShowHomePageTest do
   use ExUnit.Case
   use Hound.Helpers
 
+  import HoundSupport
+
   hound_session()
 
   test "the truth" do
@@ -9,5 +11,9 @@ defmodule ShowHomePageTest do
     assert visible_page_text() =~ "niclan"
     assert visible_page_text() =~ "a musician and a software developer highly "
      <> "interested in clean code and behavior driven development"
+
+    assert visible_link?("https://github.com/nic-lan")
+    assert visible_link?("https://www.linkedin.com/in/nicolaslanguille/")
+    assert visible_link?("https://www.instagram.com/whatever_nic_lan/")
   end
 end
