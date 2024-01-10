653c0b43a1f2a6cc878cfe54810791de471ae6ac
Nicolas Languille <nic-lan@users.noreply.github.com>
2024-01-10 14:44:25 +0100

    remove old site

--- test/hound_support.exs
+++
@@ -1,8 +0,0 @@
-defmodule HoundSupport do
-  use Hound.Helpers
-
-  def visible_link?(link) do
-    find_all_elements(:tag, "a")
-    |> Enum.any?(fn element -> attribute_value(element, "href") == link end)
-  end
-end
